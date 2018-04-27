e4469bec6dc37f51ac2a84b531add6b797df3a2d
hingston <will.hingston@gmail.com>
2018-04-27 13:30:27 +0930

    Updated to Python 3 and now complies with all PEPs

--- CheckTSP.py
+++ CheckTSP.py
@@ -10,14 +10,18 @@
 # Email: cdgamlin@gmail.com
 # -------------------------------------------------------
 
+# -------------------------------------------------------
+# Updated to Python 3 and now complies with all PEPs
+# Author: William Hingston
+# -------------------------------------------------------
+
 from sys import argv
 
 
-class Map:
+class MapRoute:
     # read and store distance map, checking for correct file structure
     def __init__(self, filename):
         # read in file in one hit
-        filename = argv[1]
         file = open(filename)
         data = file.read()
         file.close()
@@ -28,36 +32,32 @@ class Map:
         data = [[int(y) for y in x] for x in data]
 
         # remove trailing blank lines
-        while data[-1] == []:
+        while not data[-1]:
             data = data[:-1]
 
         # get number of towns, making sure the first line is structured correctly
         if len(data[0]) != 1:
-            print
-            "Error! Map file line 1 must have exactly 1 integer"
+            print("Error! Map file line 1 must have exactly 1 integer")
             exit()
-        self.numTowns = data[0][0]
+        self.num_towns = data[0][0]
 
         # check that the size of the file is correct
-        if len(data) != self.numTowns:
-            print
-            "Error!: Map file must have the same number of input lines as the number of towns"
+        if len(data) != self.num_towns:
+            print("Error!: Map file must have the same number of input lines as the number of towns")
             exit()
 
         error = False
 
         # check structure of distances between towns
-        for i in xrange(1, self.numTowns):
+        for i in range(1, self.num_towns):
             if len(data[i]) != i:
-                print
-                "Error! Map file line " + str(i + 1) + " must have exactly " + str(i) + " integer" + (
-                    "s" if i != 1 else "")
+                print("Error! Map file line " + str(i + 1) + " must have exactly " + str(i) + " integer" + (
+                    "s" if i != 1 else ""))
                 error = True
-            for j in xrange(i):
+            for j in range(i):
                 if data[i][j] < 1:
-                    print
-                    "Error! Map file line " + str(i + 1) + " integer " + str(
-                        j + 1) + " is not an integer greater than zero"
+                    print("Error! Map file line " + str(i + 1) + " integer " + str(
+                        j + 1) + " is not an integer greater than zero")
                     error = True
 
         if error:
@@ -67,37 +67,31 @@ class Map:
         self.distance = data[1:]
 
     # return distance between two towns
-    def getDist(self, i, j):
+    def get_dist(self, i, j):
         return self.distance[max(i, j) - 1][min(i, j)]
 
-    # find best path from current town through all left2visit towns using recursion
-    def bestPath(self, currentTown, left2visit):
+    # find best path from current town through all left_to_visit towns using recursion
+    def best_path(self, current_town, left_to_visit):
         # base case
-        if len(left2visit) == 1:
-            return self.getDist(currentTown, left2visit[0])
+        if len(left_to_visit) == 1:
+            return self.get_dist(current_town, left_to_visit[0])
 
         # general case
-        bestDist = float('inf')  # infinite!
-        for i in xrange(len(left2visit)):
-            goingTo = left2visit[i]
-            thenLeft2visit = left2visit[:i] + left2visit[i + 1:]
-            dist = self.getDist(currentTown, goingTo) + self.bestPath(goingTo, thenLeft2visit)
-            if dist < bestDist:
-                bestDist = dist
-        return bestDist
+        best_dist = float('inf')  # infinite!
+        for i in range(len(left_to_visit)):
+            going_to = left_to_visit[i]
+            then_left_to_visit = left_to_visit[:i] + left_to_visit[i + 1:]
+            dist = self.get_dist(current_town, going_to) + self.best_path(going_to, then_left_to_visit)
+            if dist < best_dist:
+                best_dist = dist
+        return best_dist
 
 
-### MAIN STARTS HERE ###
-
+# MAIN STARTS HERE
 if len(argv) != 2:
-    print
-    "Error!: Incorrect arguments"
-    print
-    "Usage: python checkTSP.py MAP_FILE_NAME"
+    print("Error!: Incorrect arguments")
+    print("Usage: python checkTSP.py MAP_FILE_NAME")
     exit()
 
-filename = argv[1]
-map = Map(filename)
-currentTown = 0
-left2visit = range(1, map.numTowns)
-print(map.bestPath(currentTown, left2visit))
+map_route = MapRoute(argv[1])
+print(map_route.best_path(0, list(range(1, map_route.num_towns))))
